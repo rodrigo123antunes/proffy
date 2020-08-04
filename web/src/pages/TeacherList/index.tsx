66b4537c1a3c63256d937df09064f67e8871897d
rodrigo123antunes <rodrigobpzantunes2016@gmail.com>
2020-08-03 23:09:42 -0300

    Adicionado rotas e criado compoente header.

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import backIcon from '../../assets/images/icons/back.svg';
+import logoImg from '../../assets/images/logo.svg';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css'
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponiveis">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="week-day">Dia da semana</label>
+                        <input type="text" id="week-day" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time" />
+                    </div>
+                </form>
+            </PageHeader>
+            {/* PAROU EM 2.13.53 */}
+            <main>
+
+            </main>
+        </div>
+    )
+}
+
+export default TeacherList;
